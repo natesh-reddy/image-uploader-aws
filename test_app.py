bbb3a9eeec6341fed42fb47f82bce8d570826431
natesh-reddy <natesh.reddy@sjsu.edu>
2024-03-28 00:19:57 -0700

    update db file

--- test_app.py
+++ test_app.py
@@ -11,15 +11,15 @@ def test_index(client):
     response = client.get('/')
     assert response.status_code == 200
 
-# def test_upload(client):
-#     with open('test_image.png', 'rb') as f:
-#         data = {'image': (f, 'test_image.png')}
-#         response = client.post('/upload', data=data, content_type='multipart/form-data')
-#         assert response.status_code == 200
+def test_upload(client):
+    with open('test_image.png', 'rb') as f:
+        data = {'image': (f, 'test_image.png')}
+        response = client.post('/upload', data=data, content_type='multipart/form-data')
+        assert response.status_code == 200
 
-# def test_display_responses(client):
-#     # Test displaying responses for an uploaded image
-#     response = client.get('/display-responses/test_image.png')
-#     assert response.status_code == 200
-#     assert b'Metadata for Uploaded Image' in response.data
-#     assert b'Filename:' in response.data
+def test_display_responses(client):
+    # Test displaying responses for an uploaded image
+    response = client.get('/display-responses/test_image.png')
+    assert response.status_code == 200
+    assert b'Metadata for Uploaded Image' in response.data
+    assert b'Filename:' in response.data
